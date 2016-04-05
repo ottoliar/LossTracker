5c8ede34b8e776c9c9d1792beb916f9aa64786eb
ottoliar <ottoliarobert@gmail.com>
2016-04-05 14:17:01 -0700

    Updated gulpfile and publish profiles for Azure

--- LossTracker/src/LossTracker/gulpfile.js
+++ LossTracker/src/LossTracker/gulpfile.js
@@ -3,7 +3,7 @@ var uglify = require('gulp-uglify');
 var ngAnnotate = require('gulp-ng-annotate');
 
 gulp.task('minify', function () {
-    return gulp.src("wwwroot/js/**/*.js")
+    return gulp.src(["wwwroot/js/**/*.js"])
                 .pipe(ngAnnotate())
                 .pipe(uglify())
                 .pipe(gulp.dest("wwwroot/lib/_app"));
